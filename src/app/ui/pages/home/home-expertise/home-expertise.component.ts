d6c1370574761cf0b2e7eb2017441de5d1196216
BishalJung <bishaljung63@gmail.com>
2025-09-04 20:33:46 -0400

    small changes made in the profile

--- src/app/ui/pages/home/home-expertise/home-expertise.component.ts
+++ src/app/ui/pages/home/home-expertise/home-expertise.component.ts
@@ -1,340 +1,328 @@
 import { ScrollDispatcher, ViewportRuler } from '@angular/cdk/scrolling';
-import { ChangeDetectorRef, Component, ElementRef, NgZone, OnInit, ViewChild } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  NgZone,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { MediaObserver } from '@angular/flex-layout';
-import { ReplaySubject, takeUntil, startWith, map, scan, distinctUntilChanged, takeWhile, switchMap, Observable } from 'rxjs';
-import { TRANSITION_IMAGE_SCALE, TRANSITION_TEXT } from 'src/app/ui/animations/transitions/transitions.constants';
+import {
+  ReplaySubject,
+  takeUntil,
+  startWith,
+  map,
+  scan,
+  distinctUntilChanged,
+  takeWhile,
+  switchMap,
+  Observable,
+} from 'rxjs';
+import {
+  TRANSITION_IMAGE_SCALE,
+  TRANSITION_TEXT,
+} from 'src/app/ui/animations/transitions/transitions.constants';
 import { UiUtilsView } from 'src/app/ui/utils/views.utils';
 
 @Component({
   selector: 'app-home-expertise',
   templateUrl: './home-expertise.component.html',
   styleUrls: ['./home-expertise.component.scss'],
-  animations: [
-    TRANSITION_TEXT,
-    TRANSITION_IMAGE_SCALE
-  ]
+  animations: [TRANSITION_TEXT, TRANSITION_IMAGE_SCALE],
 })
 export class HomeExpertiseComponent implements OnInit {
-
   private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
-  mOnceAnimated = false
+  mOnceAnimated = false;
 
   /* ********************************************************************************************
-    *                anims
-    */
-  _mTriggerAnim?= 'false'
+   *                anims
+   */
+  _mTriggerAnim? = 'false';
 
-  _mTriggerImage?= 'false'
+  _mTriggerImage? = 'false';
 
-
-  _mThreshold = 0.2
-
+  _mThreshold = 0.2;
 
   @ViewChild('animRefView') vAnimRefView?: ElementRef<HTMLElement>;
 
-  constructor(public el: ElementRef,
+  constructor(
+    public el: ElementRef,
     private _ngZone: NgZone,
     private cdr: ChangeDetectorRef,
     public mediaObserver: MediaObserver,
-    private scroll: ScrollDispatcher, private viewPortRuler: ViewportRuler) { }
+    private scroll: ScrollDispatcher,
+    private viewPortRuler: ViewportRuler
+  ) {}
 
-  ngOnInit(): void {
-  }
-
-
+  ngOnInit(): void {}
 
   ngAfterViewInit(): void {
     this.setupAnimation();
   }
 
   ngOnDestroy(): void {
-
-    this.destroyed$.next(true)
-    this.destroyed$.complete()
+    this.destroyed$.next(true);
+    this.destroyed$.complete();
   }
-
 
   public setupAnimation() {
     if (!this.vAnimRefView) return;
 
     // console.info("home products setupAnimation: " )
-    this.scroll.ancestorScrolled(this.vAnimRefView, 100).pipe(
-      // Makes sure to dispose on destroy
-      takeUntil(this.destroyed$),
-      startWith(0),
-      map(() => {
-        if (this.vAnimRefView != null) {
-          var visibility = UiUtilsView.getVisibility(this.vAnimRefView, this.viewPortRuler)
-          // console.log("product app-item UiUtilsView visibility: ", visibility)
-          return visibility;
+    this.scroll
+      .ancestorScrolled(this.vAnimRefView, 100)
+      .pipe(
+        // Makes sure to dispose on destroy
+        takeUntil(this.destroyed$),
+        startWith(0),
+        map(() => {
+          if (this.vAnimRefView != null) {
+            var visibility = UiUtilsView.getVisibility(
+              this.vAnimRefView,
+              this.viewPortRuler
+            );
+            // console.log("product app-item UiUtilsView visibility: ", visibility)
+            return visibility;
+          }
+          return 0;
+        }),
+        scan<number, boolean>(
+          (acc: number | boolean, val: number) =>
+            val >= this._mThreshold || (acc ? val > 0 : false)
+        ),
+        // Distincts the resulting triggers
+        distinctUntilChanged(),
+        // Stop taking the first on trigger when aosOnce is set
+        takeWhile((trigger) => {
+          // console.info("app-item  !trigger || !this.mOnceAnimated",
+          //   !trigger || !this.mOnceAnimated)
+
+          return !trigger || !this.mOnceAnimated;
+        }, true),
+        switchMap(
+          (trigger) =>
+            new Observable<number | boolean>((observer) =>
+              this._ngZone.run(() => observer.next(trigger))
+            )
+        )
+      )
+      .subscribe((val) => {
+        // console.log("home-item setupAnimation ancestorScrolled: ", val)
+
+        if (this.mOnceAnimated) {
+          return;
         }
-        return 0;
 
-      }),
-      scan<number, boolean>((acc: number | boolean, val: number) => (val >= this._mThreshold || (acc ? val > 0 : false))),
-      // Distincts the resulting triggers 
-      distinctUntilChanged(),
-      // Stop taking the first on trigger when aosOnce is set
-      takeWhile(trigger => {
-        // console.info("app-item  !trigger || !this.mOnceAnimated",
-        //   !trigger || !this.mOnceAnimated)
+        if (val) {
+          // console.log("HomeProductsComponent setupAnimation setupAnimation ancestorScrolled: ", val)
 
-        return !trigger || !this.mOnceAnimated
-      }, true),
-      switchMap(trigger => new Observable<number | boolean>(observer => this._ngZone.run(() => observer.next(trigger))))
-    ).subscribe(val => {
-
-
-      // console.log("home-item setupAnimation ancestorScrolled: ", val)
-
-      if (this.mOnceAnimated) {
-        return;
-      }
-
-      if (val) {
-        // console.log("HomeProductsComponent setupAnimation setupAnimation ancestorScrolled: ", val)
-
-        this.mOnceAnimated = true
-        this._mTriggerAnim = 'true'
-        this.cdr.detectChanges()
-      }
-      // if (this.vImageArea != null) {
-      //   var visibility = UiUtilsView.getVisibility(this.vImageArea, this.viewPortRuler)
-      //   console.log("UiUtilsView visibility: ", visibility)
-      // }
-    }
-
-    )
+          this.mOnceAnimated = true;
+          this._mTriggerAnim = 'true';
+          this.cdr.detectChanges();
+        }
+        // if (this.vImageArea != null) {
+        //   var visibility = UiUtilsView.getVisibility(this.vImageArea, this.viewPortRuler)
+        //   console.log("UiUtilsView visibility: ", visibility)
+        // }
+      });
   }
 
   _mTools = [
-
-     // "data science"
-
-     {
-      "id": "6121",
-      "name": "Python",
-      "logo": "assets/img/tools/icons8-python-96.png",
-      "link": "https://www.python.org/",
-      "tab": "data science"
+    // "data science"
+    {
+      id: '6121',
+      name: 'Python',
+      logo: 'assets/img/tools/icons8-python-96.png',
+      link: 'https://www.python.org/',
+      tab: 'data science',
     },
 
     {
-      "id": "6123",
-      "name": "Machine Learning",
-      "logo": "assets/img/tools/icons8-machine-learning-80.png",
-      "link": "https://en.wikipedia.org/wiki/Machine_learning",
-      "tab": "data science"
+      id: '6123',
+      name: 'Machine Learning',
+      logo: 'assets/img/tools/icons8-machine-learning-80.png',
+      link: 'https://en.wikipedia.org/wiki/Machine_learning',
+      tab: 'data science',
     },
     {
-      "id": "8109",
-      "name": "Statistics",
-      "logo": "assets/img/tools/icons8-statistics-80.png",
-      "link": "https://en.wikipedia.org/wiki/Statistics",
-      "tab": "web",
+      id: '8109',
+      name: 'Statistics',
+      logo: 'assets/img/tools/icons8-statistics-80.png',
+      link: 'https://en.wikipedia.org/wiki/Statistics',
+      tab: 'web',
     },
     {
-      "id": "6124",
-      "name": "Google cloud",
-      "logo": "assets/img/tools/google-cloud.png",
-      "link": "https://cloud.google.com/",
-      "tab": "data science"
+      id: '6124',
+      name: 'Google cloud',
+      logo: 'assets/img/tools/google-cloud.png',
+      link: 'https://cloud.google.com/',
+      tab: 'data science',
     },
     {
-      "id": "6125",
-      "name": "AWS Cloud Practitioner",
-      "logo": "assets/img/tools/aws-cloud.png",
-      "link": "",
-      "tab": "data science"
+      id: '6125',
+      name: 'AWS Cloud Practitioner',
+      logo: 'assets/img/tools/aws-cloud.png',
+      link: '',
+      tab: 'data science',
     },
 
     {
-      "id": "6128",
-      "name": "Tabelu",
-      "logo": "assets/img/tools/icons8-tableau-software-96.png",
-      "link": "https://www.tableau.com/",
-      "tab": "data science"
+      id: '6128',
+      name: 'Power BI',
+      logo: 'assets/img/tools/business-intelligence.png',
+      link: 'https://www.tableau.com/',
+      tab: 'data science',
     },
 
     {
-      "id": "6125",
-      "name": "EDA",
-      "logo": "assets/img/tools/icons8-exploratory-data-analysis-64.png",
-      "link": "https://www.ibm.com/topics/exploratory-data-analysis",
-      "tab": "data science"
+      id: '6125',
+      name: 'EDA',
+      logo: 'assets/img/tools/icons8-exploratory-data-analysis-64.png',
+      link: 'https://www.ibm.com/topics/exploratory-data-analysis',
+      tab: 'data science',
     },
 
     {
-      "id": "6126",
-      "name": "R-Programming",
-      "logo": "assets/img/tools/icons8-r-100.png",
-      "link": "https://www.r-project.org/about.html",
-      "tab": "data science"
-    },
-    // icons8-r-100.png
-    // android
-    {
-      "id": "9110",
-      "name": "Android",
-      "logo": "assets/img/tools/android.svg",
-      "link": "https://developer.android.com/",
-      "tab": "android",
-      "color": "#3DDC84"
+      id: '6126',
+      name: 'R-Programming',
+      logo: 'assets/img/tools/icons8-r-100.png',
+      link: 'https://www.r-project.org/about.html',
+      tab: 'data science',
     },
     {
-      "id": "9112",
-      "name": "SQL",
-      "logo": "assets/img/tools/icons8-sql-80.png",
-      "link": "https://aws.amazon.com/what-is/sql/#:~:text=Structured%20query%20language%20(SQL)%20is,relationships%20between%20the%20data%20values.",
-      "tab": "android"
+      id: '9112',
+      name: 'SQL',
+      logo: 'assets/img/tools/icons8-sql-80.png',
+      link: 'https://aws.amazon.com/what-is/sql/#:~:text=Structured%20query%20language%20(SQL)%20is,relationships%20between%20the%20data%20values.',
+      tab: 'android',
     },
     {
-      "id": "9113",
-      "name": "RxJS",
-      "logo": "assets/img/tools/RxJava-logo.png",
-      "link": "http://reactivex.io/",
-      "tab": "android"
+      id: '9114',
+      name: 'Matplotlib',
+      logo: 'assets/img/tools/Matplotlib_icon.svg',
+      link: 'https://matplotlib.org/',
+      tab: 'data science',
     },
     {
-      "id": "9114",
-      "name": "Matplotlib",
-      "logo": "assets/img/tools/Matplotlib_icon.svg",
-      "link": "https://matplotlib.org/",
-      "tab": "data science"
+      id: '9115',
+      name: 'Seaborn',
+      logo: 'assets/img/tools/seaborn-seeklogo.svg',
+      link: 'https://seaborn.pydata.org/index.html',
+      tab: 'android',
     },
     {
-      "id": "9115",
-      "name": "Seaborn",
-      "logo": "assets/img/tools/seaborn-seeklogo.svg",
-      "link": "https://seaborn.pydata.org/index.html",
-      "tab": "android"
+      id: '9116',
+      name: 'Kotlin',
+      logo: 'assets/img/tools/kotlin-logo.png',
+      link: 'https://kotlinlang.org/',
+      tab: 'android',
     },
     {
-      "id": "9116",
-      "name": "Kotlin",
-      "logo": "assets/img/tools/kotlin-logo.png",
-      "link": "https://kotlinlang.org/",
-      "tab": "android"
+      id: '9004',
+      name: 'PySpark',
+      logo: 'assets/img/tools/pyspark.svg',
+      link: 'https://spark.apache.org/docs/latest/api/python/',
+      tab: 'programming',
     },
-
     // cross
-{
-      "id": "4101",
-      "name": "No SQL",
-      "logo": "assets/img/tools/icons8-no-sql-64.png",
-      "link": "https://www.ibm.com/topics/nosql-databases#:~:text=NoSQL%2C%20also%20referred%20to%20as,structures%20found%20in%20relational%20databases.",
-      "tab": "Cross",
-      "color": "#42A5F5"
+    {
+      id: '4101',
+      name: 'No SQL',
+      logo: 'assets/img/tools/icons8-no-sql-64.png',
+      link: 'https://www.ibm.com/topics/nosql-databases#:~:text=NoSQL%2C%20also%20referred%20to%20as,structures%20found%20in%20relational%20databases.',
+      tab: 'Cross',
+      color: '#42A5F5',
+    },
+    {
+      id: '9010',
+      name: 'Apache Airflow',
+      logo: 'assets/img/tools/airflow.svg',
+      link: 'https://airflow.apache.org/',
+      tab: 'etl',
+    },
+    {
+      id: '9011',
+      name: 'Snowflake',
+      logo: 'assets/img/tools/snowflake.svg',
+      link: 'https://www.snowflake.com/',
+      tab: 'etl',
+    },
+    {
+      id: '9009',
+      name: 'AWS Glue',
+      logo: 'assets/img/tools/aws-glue.svg',
+      link: 'https://aws.amazon.com/glue/',
+      tab: 'etl',
     },
 
-    // web
-
-    
-
     {
-      "id": "8102",
-      "name": "HTML",
-      "logo": "assets/img/tools/icons8-html-96.png",
-      "link": "https://www.w3schools.com/html/",
-      "tab": "web"
-    },
-
-    {
-      "id": "8103",
-      "name": "WebComponents",
-      "logo": "assets/img/tools/web-component-logo.png",
-      "link": "https://www.webcomponents.org/",
-      "tab": "web"
+      id: '9007',
+      name: 'Excel',
+      logo: 'assets/img/tools/excel.svg',
+      link: 'https://www.microsoft.com/en-us/microsoft-365/excel',
+      tab: 'analytics',
     },
     {
-      "id": "8105",
-      "name": "Bootstrap",
-      "logo": "assets/img/tools/icons8-bootstrap-96.png",
-      "link": "https://getbootstrap.com/",
-      "tab": "web"
+      id: '8106',
+      name: 'D3 js',
+      logo: 'assets/img/tools/d3.svg',
+      link: 'https://d3js.org/',
+      tab: 'web',
     },
-    {
-      "id": "8108",
-      "name": "Sass",
-      "logo": "assets/img/tools/sass-logo.svg",
-      "link": "https://sass-lang.com/",
-      "tab": "web",
-      "color": "#CF649A"
-    },
-    {
-      "id": "8106",
-      "name": "D3 js",
-      "logo": "assets/img/tools/d3.svg",
-      "link": "https://d3js.org/",
-      "tab": "web"
-    },
-    {
-      "id": "8104",
-      "name": "Ngrx",
-      "logo": "assets/img/tools/ngrx.svg",
-      "link": "https://ngrx.io/",
-      "tab": "web"
-    },
-    {
-      "id": "8101",
-      "name": "Angular",
-      "logo": "assets/img/tools/angular.png",
-      "link": "https://angular.io/",
-      "tab": "web",
-      "color": "#FF4369"
-    },
-   
-
     // backend
 
     {
-      "id": "7121",
-      "name": "Scrum",
-      "logo": "assets/img/tools/icons8-scrum-64.png",
-      "link": "https://www.scrum.org/resources/what-scrum-module",
-      "tab": "back-end"
+      id: '7121',
+      name: 'Scrum',
+      logo: 'assets/img/tools/icons8-scrum-64.png',
+      link: 'https://www.scrum.org/resources/what-scrum-module',
+      tab: 'back-end',
     },
     {
-      "id": "7122",
-      "name": "Trello",
-      "logo": "assets/img/tools/icons8-trello-96.png",
-      "link": "https://trello.com/?aceid&adposition&adgroup=143241824842&campaign=18406634139&creative=633330905204&device=c&keyword=trello&matchtype=e&network=g&placement&ds_kids=p73316792581&ds_e=GOOGLE&ds_eid=700000001557344&ds_e1=GOOGLE&gad_source=1",
-      "tab": "back-end"
+      id: '7122',
+      name: 'Trello',
+      logo: 'assets/img/tools/icons8-trello-96.png',
+      link: 'https://trello.com/?aceid&adposition&adgroup=143241824842&campaign=18406634139&creative=633330905204&device=c&keyword=trello&matchtype=e&network=g&placement&ds_kids=p73316792581&ds_e=GOOGLE&ds_eid=700000001557344&ds_e1=GOOGLE&gad_source=1',
+      tab: 'back-end',
+    },
+
+    // design
+    {
+      id: '5131',
+      name: 'Figma',
+      logo: 'assets/img/tools/figma.svg',
+      link: 'https://www.figma.com/',
+      tab: 'design',
+    },
+    {
+      id: '5132',
+      name: 'Adobe XD',
+      logo: 'assets/img/tools/xd.png',
+      link: 'https://www.adobe.com/products/xd.html',
+      tab: 'design',
     },
 
     {
-      "id": "7126",
-      "name": "NodeJs",
-      "logo": "assets/img/tools/nodejs.png",
-      "link": "https://nodejs.org/en/",
-      "tab": "back-end"
+      id: '5133',
+      name: 'Adobe Photoshop',
+      logo: 'assets/img/tools/ps.png',
+      link: 'https://www.adobe.com/products/photoshop.html',
+      tab: 'design',
     },
-
-  
- // design
- {
-  "id": "5131",
-  "name": "Figma",
-  "logo": "assets/img/tools/figma.svg",
-  "link": "https://www.figma.com/",
-  "tab": "design"
-},
-{
-  "id": "5132",
-  "name": "Adobe XD",
-  "logo": "assets/img/tools/xd.png",
-  "link": "https://www.adobe.com/products/xd.html",
-  "tab": "design"
-},
-
-{
-  "id": "5133",
-  "name": "Adobe Photoshop",
-  "logo": "assets/img/tools/ps.png",
-  "link": "https://www.adobe.com/products/photoshop.html",
-  "tab": "design"
-},
-
-  ]
-
+    {
+      id: '9016',
+      name: 'JIRA',
+      logo: 'assets/img/tools/jira.svg',
+      link: 'https://www.atlassian.com/software/jira',
+      tab: 'collaboration',
+    },
+    {
+      id: '9017',
+      name: 'Confluence',
+      logo: 'assets/img/tools/confluence.svg',
+      link: 'https://www.atlassian.com/software/confluence',
+      tab: 'collaboration',
+    },
+  ];
 }
--- src/app/ui/pages/home/home-showcases/my-apps/my-apps.component.ts
+++ src/app/ui/pages/home/home-showcases/my-apps/my-apps.component.ts
@@ -1,180 +1,202 @@
 import { ScrollDispatcher, ViewportRuler } from '@angular/cdk/scrolling';
-import { ChangeDetectorRef, Component, ElementRef, NgZone, OnInit, ViewChild } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  NgZone,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { MediaObserver } from '@angular/flex-layout';
 import { FormBuilder } from '@angular/forms';
-import { ReplaySubject, takeUntil, startWith, map, scan, distinctUntilChanged, takeWhile, switchMap, Observable } from 'rxjs';
+import {
+  ReplaySubject,
+  takeUntil,
+  startWith,
+  map,
+  scan,
+  distinctUntilChanged,
+  takeWhile,
+  switchMap,
+  Observable,
+} from 'rxjs';
 import { AppType } from 'src/app/types/apps_type';
-import { TRANSITION_IMAGE_SCALE, TRANSITION_TEXT } from 'src/app/ui/animations/transitions/transitions.constants';
+import {
+  TRANSITION_IMAGE_SCALE,
+  TRANSITION_TEXT,
+} from 'src/app/ui/animations/transitions/transitions.constants';
 import { UiUtilsView } from 'src/app/ui/utils/views.utils';
 
 @Component({
   selector: 'app-my-apps',
   templateUrl: './my-apps.component.html',
   styleUrls: ['./my-apps.component.scss'],
-  animations: [
-    TRANSITION_TEXT,
-    TRANSITION_IMAGE_SCALE
-  ]
+  animations: [TRANSITION_TEXT, TRANSITION_IMAGE_SCALE],
 })
 export class MyAppsComponent implements OnInit {
-
-
-  _mApps : AppType[] = [
-  
+  _mApps: AppType[] = [
     {
-      "id": "5133",
-      "name": "Early Heart Disease Classification",
-      "image": "../../../../../../assets/img/avatar/dataset-card.jpg",
-      "link": "https://github.com/BishalJung7377/heart_disease_classification",
-      "tab": "ML",
-      "caption": "Predicting Heart Disease Risk Using Machine Learning",
-      "isFull": false,
-      "primary":"#E1E1E1",
-      "background":"#2c698d"
+      id: '5133',
+      name: 'Early Heart Disease Classification',
+      image: '../../../../../../assets/img/avatar/dataset-card.jpg',
+      link: 'https://github.com/BishalJung7377/heart_disease_classification',
+      tab: 'ML',
+      caption: 'Predicting Heart Disease Risk Using Machine Learning',
+      isFull: false,
+      primary: '#E1E1E1',
+      background: '#2c698d',
     },
     {
-     "id": "5131",
-     "name": "Sentiment Analysis",
-     "image": "../../../../../../assets/img/avatar/sentiment analysis.png",
-     "link": "https://github.com/BishalJung7377/sentiment_analysis_natural_disaster_tweets",
-     "tab": "Sentiment",
-     "caption": "ML and NLP for Sentiments Analysis",
-     "isFull": false,
-     "primary":"#61dafb",
-     "background":"#E1E1E1"
-   },
-   {
-     "id": "5132",
-     "name": "Vetmentsfashion Nepal",
-     "image": "../../../../../../assets/img/avatar/mobile.png",
-     "link": "https://github.com/BishalJung7377/VetementsFashionNepal",
-     "tab": "Android",
-     "isFull": false,
-     "caption": "In Android(Kotlin) -",
-     "primary" : "#3CE79F",
-     "background":"#E1E1E1"
-   },
-   {
-     "id": "5133",
-     "name": "Shoe Price Predictior",
-     "image": "../../../../../../assets/img/avatar/shoepriceprediction.webp",
-     "link": "https://www.kaggle.com/code/bishaljungthapa/shoe-price-eda",
-     "tab": "Android",
-     "caption": "Python (EDA) -",
-     "isFull": false,
-     "background":"#3CE79F"
-   }
-  //  {
-  //   "id": "5133",
-  //   "name": "Shoe Price Predictior",
-  //   "image": "../../../../../../assets/img/avatar/bishal_avatar.jpg",
-  //   "link": "https://www.kaggle.com/code/bishaljungthapa/shoe-price-eda",
-  //   "tab": "Android",
-  //   "caption": "Python (EDA) -",
-  //   "isFull": true,
-  //   "background":"#3CE79F"
-  // }
+      id: '5131',
+      name: 'Sentiment Analysis',
+      image: '../../../../../../assets/img/avatar/sentiment analysis.png',
+      link: 'https://github.com/BishalJung7377/sentiment_analysis_natural_disaster_tweets',
+      tab: 'Sentiment',
+      caption: 'ML and NLP for Sentiments Analysis',
+      isFull: false,
+      primary: '#61dafb',
+      background: '#E1E1E1',
+    },
+    {
+      id: '5132',
+      name: 'Customer Churn Prediction',
+      image: '../../../../../../assets/img/avatar/mobile.png',
+      link: 'https://github.com/takeo-quantumleap/bda74-capstone-project-customer-churn-prediction-for-t-mobile-bda74capstone-s798388',
+      tab: 'Churn Prediction',
+      isFull: false,
+      caption: 'Customer Churn Prediction for T-Mobile',
+      background: '#3CE79F',
+    },
+    {
+      id: '5133',
+      name: 'Loan Status Prediction',
+      image: '../../../../../../assets/img/avatar/shoepriceprediction.webp',
+      link: 'https://github.com/takeo-quantumleap/bda74-feature-forge-enhancing-evaluating-ml-models-bda74feature-forge-s798388',
+      tab: 'Android',
+      caption: 'Automating Loan Approval Decisions with ML',
+      isFull: false,
+      primary: '#3CE79F',
+      background: '#E1E1E1',
+    },
+    {
+      id: '5133',
+      name: 'Yellow Taxi ETL & Analytics',
+      image: '../../../../../../assets/img/avatar/shoepriceprediction.webp',
+      link: 'https://github.com/takeo-quantumleap/bda74-new-york-city-yellow-cab-processing-large-datasets-bda74-newyork-s798388/tree/main',
+      tab: 'Android',
+      caption: 'NYC Yellow Taxi Lakehouse',
+      isFull: false,
+      primary: '#61dafb',
+      background: '#E1E1E1',
+    },
+    {
+      id: '5133',
+      name: 'Design, ETL, Modeling, and Business Insights',
+      image: '../../../../../../assets/img/avatar/shoepriceprediction.webp',
+      link: '',
+      tab: 'Android',
+      caption: 'Modern Data Warehouse Project: From Architecture to Analytics',
+      isFull: false,
+      background: '#3CE79F',
+    },
   ];
 
   private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
-  mOnceAnimated = false
+  mOnceAnimated = false;
 
   /* ********************************************************************************************
-    *                anims
-    */
-  _mTriggerAnim?= 'false'
+   *                anims
+   */
+  _mTriggerAnim? = 'false';
 
-
-
-  _mThreshold = 0.4
-
+  _mThreshold = 0.4;
 
   @ViewChild('animRefView') vAnimRefView?: ElementRef<HTMLElement>;
 
-  constructor(public el: ElementRef,
+  constructor(
+    public el: ElementRef,
     private _ngZone: NgZone,
     private cdr: ChangeDetectorRef,
     public mediaObserver: MediaObserver,
-    private scroll: ScrollDispatcher, private viewPortRuler: ViewportRuler,
-    private formBuilder: FormBuilder) {
+    private scroll: ScrollDispatcher,
+    private viewPortRuler: ViewportRuler,
+    private formBuilder: FormBuilder
+  ) {}
 
-
-
-  }
-
-  ngOnInit(): void {
-  }
-
-
+  ngOnInit(): void {}
 
   ngAfterViewInit(): void {
     this.setupAnimation();
   }
 
   ngOnDestroy(): void {
-
-    this.destroyed$.next(true)
-    this.destroyed$.complete()
+    this.destroyed$.next(true);
+    this.destroyed$.complete();
   }
-
-
-
 
   /* ***************************************************************************
    *                                  other parts
    */
 
-
   public setupAnimation() {
     if (!this.vAnimRefView) return;
 
     // console.info("home products setupAnimation: " )
-    this.scroll.ancestorScrolled(this.vAnimRefView, 100).pipe(
-      // Makes sure to dispose on destroy
-      takeUntil(this.destroyed$),
-      startWith(0),
-      map(() => {
-        if (this.vAnimRefView != null) {
-          var visibility = UiUtilsView.getVisibility(this.vAnimRefView, this.viewPortRuler)
-          // console.log("product app-item UiUtilsView visibility: ", visibility)
-          return visibility;
+    this.scroll
+      .ancestorScrolled(this.vAnimRefView, 100)
+      .pipe(
+        // Makes sure to dispose on destroy
+        takeUntil(this.destroyed$),
+        startWith(0),
+        map(() => {
+          if (this.vAnimRefView != null) {
+            var visibility = UiUtilsView.getVisibility(
+              this.vAnimRefView,
+              this.viewPortRuler
+            );
+            // console.log("product app-item UiUtilsView visibility: ", visibility)
+            return visibility;
+          }
+          return 0;
+        }),
+        scan<number, boolean>(
+          (acc: number | boolean, val: number) =>
+            val >= this._mThreshold || (acc ? val > 0 : false)
+        ),
+        // Distincts the resulting triggers
+        distinctUntilChanged(),
+        // Stop taking the first on trigger when aosOnce is set
+        takeWhile((trigger) => {
+          // console.info("app-item  !trigger || !this.mOnceAnimated",
+          //   !trigger || !this.mOnceAnimated)
+
+          return !trigger || !this.mOnceAnimated;
+        }, true),
+        switchMap(
+          (trigger) =>
+            new Observable<number | boolean>((observer) =>
+              this._ngZone.run(() => observer.next(trigger))
+            )
+        )
+      )
+      .subscribe((val) => {
+        // console.log("home-item setupAnimation ancestorScrolled: ", val)
+
+        if (this.mOnceAnimated) {
+          return;
         }
-        return 0;
 
-      }),
-      scan<number, boolean>((acc: number | boolean, val: number) => (val >= this._mThreshold || (acc ? val > 0 : false))),
-      // Distincts the resulting triggers 
-      distinctUntilChanged(),
-      // Stop taking the first on trigger when aosOnce is set
-      takeWhile(trigger => {
-        // console.info("app-item  !trigger || !this.mOnceAnimated",
-        //   !trigger || !this.mOnceAnimated)
+        if (val) {
+          // console.log("HomeProductsComponent setupAnimation setupAnimation ancestorScrolled: ", val)
 
-        return !trigger || !this.mOnceAnimated
-      }, true),
-      switchMap(trigger => new Observable<number | boolean>(observer => this._ngZone.run(() => observer.next(trigger))))
-    ).subscribe(val => {
-
-
-      // console.log("home-item setupAnimation ancestorScrolled: ", val)
-
-      if (this.mOnceAnimated) {
-        return;
-      }
-
-      if (val) {
-        // console.log("HomeProductsComponent setupAnimation setupAnimation ancestorScrolled: ", val)
-
-        this.mOnceAnimated = true
-        this._mTriggerAnim = 'true'
-        this.cdr.detectChanges()
-      }
-      // if (this.vImageArea != null) {
-      //   var visibility = UiUtilsView.getVisibility(this.vImageArea, this.viewPortRuler)
-      //   console.log("UiUtilsView visibility: ", visibility)
-      // }
-    }
-
-    )
+          this.mOnceAnimated = true;
+          this._mTriggerAnim = 'true';
+          this.cdr.detectChanges();
+        }
+        // if (this.vImageArea != null) {
+        //   var visibility = UiUtilsView.getVisibility(this.vImageArea, this.viewPortRuler)
+        //   console.log("UiUtilsView visibility: ", visibility)
+        // }
+      });
   }
 }
